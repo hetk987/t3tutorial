831ecd91f5a1800790552b23177fdc062cc1ab3c
hetk987 <hetuniverse@gmail.com>
2024-05-16 19:08:20 -0500

    Scaffold Basic UI

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 ## TODO
 
-- [ ] Make it deploy
-- [ ] Scaffold basic uy with mock data
+- [x] Make it deploy
+- [x] Scaffold basic ui with mock data
 - [ ] Actually set up a database
 - [ ] Attatch database to UI
-- [ ] Add authentication 
-- [ ] Add image upload 
+- [ ] Add authentication
+- [ ] Add image upload
 - [ ] Error Management (w/ Sentry)
 - [ ] Routing/image page (parrallel route)
 - [ ] Delete button (w/ Server Actions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+
+const mcokURLs = [
+  "https://utfs.io/f/b7eb508b-b2dc-452d-bcfe-135a8a986680-ueb5b5.jpg",
+  "https://utfs.io/f/18ae7e45-520f-40b7-8be2-19ac124f6065-9mlv9c.webp",
+  "https://utfs.io/f/73e45415-3d24-43db-bd12-bf9c3612676e-bz8spu.jpg",
+  "https://utfs.io/f/7916b9a7-77ba-4956-b905-54250999f574-kp8oao.jpg"
+];
+
+const mockImages = mcokURLs.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (Tutorial in Progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
