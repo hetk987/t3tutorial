a84fc67d738792807c3ed4453be6b3b14fbb800b
hetk987 <hetuniverse@gmail.com>
2024-05-23 13:27:43 -0500

    more clean up on modal, fix text, fix layout

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -5,5 +5,13 @@ export default async function FullPageImageView(props: { id: number }) {
 
     const image = await getImage(props.id);
 
-    return <img src={image.url} className="w-96" />;
+    return (
+        <div className="flex w-full h-full min-w-0">
+            <div className="flex-shrink flex justify-items items-center">
+                <img src={image.url} className="w-96 object-contain " />
+            </div>
+            <div className="flex w-48 flex-col flex-shrink-0 border-l">
+                <div className="text-xl font-bold ">{image.name}</div>
+            </div>
+        </div>);
 }
